458b6936894d102e014fa48c5e4352405a53033f
yasithKumara <yasithkumara@gmail.com>
2023-05-29 23:24:43 +0300

    bugged swiper

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import PrivateRoute from './components/PrivateRoute';
 import Category from './pages/Category';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
           <Route path='/sign-up' element={<SignUp/>} />
           <Route path='/create-listing' element={<CreateListing/>} />
           <Route path='/category/:categoryName/:listingId' element={<Listing/>} />
+          <Route path='/contact/:landlordId' element={<Contact/>} />
 
         </Routes>
         {/* because we have to use usenavigate we are keeping this in the router */}
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
 import shareIcon from "../assets/svg/shareIcon.svg";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { getAuth } from "firebase/auth";
 import { getDoc, doc } from "firebase/firestore";
 import { db } from "../firebase.config";
+import Spinner from "../components/Spinner";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+//import "swiper/swiper-bundle.css";
+import "swiper/swiper-bundle.css";
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 function Listing() {
   const [listing, setListing] = useState(null);
@@ -19,9 +26,9 @@ function Listing() {
     const fetchListing = async () => {
       try {
         const docRef = doc(db, "listings", params.listingId);
-        console.log('first')
+        console.log("first");
         const docSnap = await getDoc(docRef);
-        console.log('second')
+        console.log("second");
 
         if (docSnap.exists()) {
           console.log(docSnap.data());
@@ -36,7 +43,91 @@ function Listing() {
     fetchListing();
   }, [navigate, params.listingId]);
 
-  return <div>Listing</div>;
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <main>
+        {/* <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+          {listing.imageUrls.map((url, index) => (
+            <SwiperSlide key={index}>
+              <div
+                className="swiperSlideDiv"
+                style={{
+                  background: `url(${listing.imageUrls[index]}) center no-repeat`,
+                  backgroundSize: "cover",
+                }}
+              ></div>
+            </SwiperSlide>
+          ))}
+        </Swiper> */}
+
+        <div
+          className="shareIconDiv"
+          onClick={() => {
+            navigator.clipboard.writeText(window.location.href);
+            setShareLinkCopied(true);
+            setTimeout(() => {
+              setShareLinkCopied(false);
+            }, 2000);
+          }}
+        >
+          <img src={shareIcon} alt="share icon" />
+        </div>
+
+        {shareLinkCopied && <p className="linkCopied">Link Copied!</p>}
+
+        <div className="listingDetails">
+          <p className="listingName">
+            {listing.name} - $
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </p>
+          <p className="listingLocation">{listing.location}</p>
+          <p className="listingType">
+            For {listing.type === "rent" ? "Rent" : "Sale"}
+          </p>
+          {listing.offer && (
+            <p className="discountPrice">
+              ${listing.regularPrice - listing.discountedPrice} discount
+            </p>
+          )}
+          <ul className="listingDetailsList">
+            <li>
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} Bedrooms`
+                : "1 Bedroom"}
+            </li>
+            <li>
+              {listing.bedrooms > 1
+                ? `${listing.bathrooms} Bathrooms`
+                : "1 Bathroom"}
+            </li>
+            <li>{listing.parking && "Parking Spot"}</li>
+            <li>{listing.furnished && "Furnished"}</li>
+          </ul>
+          <p className="listingLocationTitle">Location</p>
+          {/* map */}
+
+          {auth.currentUser?.uid !== listing.userRef && (
+            <Link
+              to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+              className="primaryButton"
+            >
+              Contact Landlord
+            </Link>
+          )}
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default Listing;
